28e5631ac1b01595eae7db26a891e8e466672ead
aceair7 <alvaromramos@gmail.com>
2025-11-21 21:03:19 -0300

    comentarios quase prontos (com ford-fulkerson)

--- abb.h
+++ abb.h
@@ -4,7 +4,11 @@ typedef struct produto Produto;
 typedef struct caminho_item CaminhoItem; // define as estruturas
 
 Arvore* criarABB(); // cria Arvore Binaria de Busca
+
 int adicionar(Arvore *arvore, float custo, int *caminho, int tamanho); // insere nó
+
 No *buscar(Arvore *arvore, float custo); // busca um caminho pelo custo
+
 void encerrarArvore(Arvore *arvore); // Encerra e libera
+
 void buscarIntervalo(Arvore *arvore, float min, float max); // busca caminhos num intervalo de custo
--- pq.h
+++ pq.h
@@ -5,9 +5,12 @@ typedef struct filaprio{
     int tam_max; // tamanho máx
 } FilaPrio;
 
-
 FilaPrio *create_pq(int tam_max); // cria fila de prioridade com um espaço máximo
+
 void destroy_pq(FilaPrio *pq); // libera fila de prioridade
+
 int pq_vazia(FilaPrio *pq); // verifica se está vazia
+
 void insere(FilaPrio *pq, int id, float prioridade); // insere na fila de prioridade, com um determinado identificador e prioridade
+
 int remove_min(FilaPrio *pq); // remove o item com menor prioridade
